7edfdbffd00fd95907d7aee8fc1ec811e93dfb73
ujjwal-etech <ujjawal@excellencetechnologies.in>
2015-12-03 17:54:21 +0530

    Chat Home page (User Data from Parse database) #issue 4

--- www/app/home/home.controller.js
+++ www/app/home/home.controller.js
@@ -2,45 +2,26 @@
     'use strict';
 
     angular.module('starter')
-        .controller('homeController', homeController);
 
-    function homeController($scope, $state, googleLogin) {
-        $scope.lists = [{
-            "name": "User",
-            "image": "img/avatar.jpg",
-            "msg": "Hii !! How Are You"
-        }, {
-            "name": "User",
-            "image": "img/avatar.jpg",
-            "msg": "Hii !! How Are You"
-        }, {
-            "name": "User",
-            "image": "img/avatar.jpg",
-            "msg": "Hii !! How Are You"
-        }, {
-            "name": "User",
-            "image": "img/avatar.jpg",
-            "msg": "Hii !! How Are You"
-        }, {
-            "name": "User",
-            "image": "img/avatar.jpg",
-            "msg": "Hii !! How Are You"
-        }, {
-            "name": "User",
-            "image": "img/avatar.jpg",
-            "msg": "Hii !! How Are You"
-        },{
-            "name": "User",
-            "image": "img/avatar.jpg",
-            "msg": "Hii !! How Are You"
-        },{
-            "name": "User",
-            "image": "img/avatar.jpg",
-            "msg": "Hii !! How Are You"
-        },{
-            "name": "User",
-            "image": "img/avatar.jpg",
-            "msg": "Hii !! How Are You"
-        }];
+    .controller('homeController', homeController);
+
+    function homeController($scope, $rootScope, $state, googleLogin) {
+        Parse.initialize("028aHh8C4DIPmPkE7mt505yeeUGFm6DtWFgH9pUY", "xQvPBzwOHswmTxPRWhgd5pUU3DREU1AtS3cBTJns");
+        var q2 = new Parse.Query(Parse.User);
+        q2.find({
+            success: function(items) {
+                var strin = [];
+                console.log(items.length);
+                for (var i = 0; i < items.length - 1; i++) {
+                    strin[i] = items[i];
+                }
+                var arr = [];
+                arr = JSON.stringify(strin);
+                $scope.lists = JSON.parse(arr);
+                $scope.$apply();
+                console.log($scope.lists);
+            }
+        });
+        console.log($scope.lists);
     }
 })();
