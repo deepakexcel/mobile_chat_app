09d0f7a0669a00079c27cf222d54f9d4c21c00d7
yuvraj <yuvraj@excellencetechnologies.in>
2015-12-03 15:18:42 +0530

    Done. Add Skip option On Login Page #1

--- www/app/home/home.service.js
+++ www/app/home/home.service.js
@@ -2,11 +2,21 @@
     'use strict';
     angular.module('starter')
             .factory('homeService', homeService);
-    
+
     function homeService($window, $localStorage) {
         return {
             set: function(key, value) {
                 $window.localStorage.setItem(key, value);
+            },
+            fakeLogin: function() {
+                var user = {
+                    name: faker.name.findName(),
+                    user_id: faker.random.uuid(),
+//                email: faker.internet.email(),
+//                image: faker.image.avatar()
+                };
+                this.set('user_name', user.name);
+                this.set('user_id', user.user_id);
             }
         }
     }
